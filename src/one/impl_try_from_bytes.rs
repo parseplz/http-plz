5a70384eb5f270f3ef4a552669c5a4faa9b43b37
darkseid <gh0stv3g3ta@gmail.com>
2026-04-13 16:54:38 +0530

    fix: MsgParseErr contains entire unparsable message

--- src/one/impl_try_from_bytes.rs
+++ src/one/impl_try_from_bytes.rs
@@ -3,65 +3,103 @@ use bytes::BytesMut;
 use decompression_plz::DecompressTrait;
 use header_plz::{
     OneInfoLine, OneMessageHead, abnf::HEADER_DELIMITER,
-    body_headers::parse::ParseBodyHeaders, const_headers::CONTENT_LENGTH,
+    body_headers::parse::ParseBodyHeaders,
+    message_head::error::MessageHeadError,
 };
+use thiserror::Error;
 
-use crate::one::{OneOne, parse::error::BuildMessageError};
+use crate::one::OneOne;
 
-/* Description:
- *      Build oneone from BytesMut.
- *      Used when request/response is modified in interceptor. No chunked body,
- *      as chunked is converted to Content-Length by convert_one_dot_one()
- *
- * Steps:
- *      1. Find HEADER_DELIMITER (2 * CRLF) in buf.
- *      2. Split buf at index.
- *      3. Build OneOne.
- *      4. if buf !empty, i.e. body is present.
- *          a. set body.
- *          b. If content-length header is present, update content-length by calling
- *          update_content_length().
- *          c. Else add, new content-length header.
- *
- * Error:
- *      BuildFrameError::UnableToFindCRLF  [1]
- *      BuildFrameError::HttpDecodeError   [3]
- */
+#[derive(Debug, Error)]
+#[error("message parse err| {}", self.kind)]
+pub struct MsgParseErr {
+    bytes: BytesMut,
+    kind: MsgParseErrKind,
+}
+
+impl MsgParseErr {
+    pub fn crlf(bytes: BytesMut) -> Self {
+        Self {
+            bytes,
+            kind: MsgParseErrKind::UnableToFindCRLF,
+        }
+    }
+
+    pub fn message_head(bytes: BytesMut, err: MessageHeadError) -> Self {
+        Self {
+            bytes,
+            kind: MsgParseErrKind::MessageHead(err),
+        }
+    }
+
+    pub fn into_bytes(self) -> BytesMut {
+        use MsgParseErrKind::*;
+        match self.kind {
+            UnableToFindCRLF => self.bytes,
+            MessageHead(err) => {
+                let mut head = err.into_bytes();
+                head.unsplit(self.bytes);
+                head
+            }
+        }
+    }
+}
+
+#[derive(Debug, Error)]
+pub enum MsgParseErrKind {
+    #[error("Failed to FindCRLF")]
+    UnableToFindCRLF,
+    #[error("Failed to DecodeHTTP| {0}")]
+    MessageHead(MessageHeadError),
+}
 
 impl<T> TryFrom<BytesMut> for OneOne<T>
 where
     T: OneInfoLine + std::fmt::Debug,
     OneMessageHead<T>: ParseBodyHeaders,
 {
-    type Error = BuildMessageError;
+    type Error = MsgParseErr;
 
     fn try_from(mut buf: BytesMut) -> Result<Self, Self::Error> {
-        let index = buf
-            .windows(4)
-            .position(|window| window == HEADER_DELIMITER)
-            .ok_or(BuildMessageError::UnableToFindCRLF)?;
+        let Some(index) =
+            buf.windows(4).position(|window| window == HEADER_DELIMITER)
+        else {
+            return Err(MsgParseErr::crlf(buf));
+        };
         let message_head = buf.split_to(index + HEADER_DELIMITER.len());
-        let mut one = OneOne::try_from_message_head_buf(message_head)?;
+        let mut one = match OneOne::try_from_message_head_buf(message_head) {
+            Ok(one) => one,
+            Err(e) => return Err(MsgParseErr::message_head(buf, e)),
+        };
         if !buf.is_empty() {
-            let len = buf.len().to_string();
             one.set_body(Body::Raw(buf));
-            if !one.update_header_value_on_key(CONTENT_LENGTH, len.as_bytes())
-            {
-                one.insert_header(CONTENT_LENGTH, len.as_bytes());
-            }
         }
         Ok(one)
+    }
+}
+
+impl<T> TryFrom<&[u8]> for OneOne<T>
+where
+    T: OneInfoLine + std::fmt::Debug,
+    OneMessageHead<T>: ParseBodyHeaders,
+{
+    type Error = MsgParseErr;
+
+    fn try_from(value: &[u8]) -> Result<Self, Self::Error> {
+        Self::try_from(BytesMut::from(value))
     }
 }
 
 #[cfg(test)]
 mod tests {
     use crate::{OneRequest, OneResponse};
+    use rstest::rstest;
 
     use super::*;
 
+    // request
     #[test]
-    fn test_request_try_from_bytes_content_length_no_body() {
+    fn test_request_try_from_bytes_no_body() {
         let req = "POST / HTTP/1.1\r\n\r\n";
         let input = BytesMut::from(req);
         let result = OneRequest::try_from(input).unwrap();
@@ -69,66 +107,55 @@ mod tests {
     }
 
     #[test]
-    fn test_request_try_from_bytes_content_length_no_cl() {
-        let input = BytesMut::from("POST / HTTP/1.1\r\n\r\nHello");
+    fn test_request_try_from_bytes_no_cl_body() {
+        let req = "POST / HTTP/1.1\r\n\r\nHello";
+        let input = BytesMut::from(req);
         let result = OneRequest::try_from(input).unwrap();
-        let verify = BytesMut::from(
-            "POST / HTTP/1.1\r\ncontent-length: 5\r\n\r\nHello",
-        );
-        assert_eq!(result.into_bytes(), verify);
+        assert_eq!(result.into_bytes(), req);
     }
 
     #[test]
-    fn test_request_try_from_bytes_content_length_less() {
-        let input =
-            BytesMut::from("POST / HTTP/1.1\r\nContent-Length: 10\r\n\r\na");
+    fn test_request_try_from_bytes_cl_body() {
+        let req = "POST / HTTP/1.1\r\nContent-Length: 10\r\n\r\na";
+        let input = BytesMut::from(req);
         let result = OneRequest::try_from(input).unwrap();
-        let verify =
-            BytesMut::from("POST / HTTP/1.1\r\nContent-Length: 1\r\n\r\na");
-        assert_eq!(result.into_bytes(), verify);
+        assert_eq!(result.into_bytes(), req);
     }
 
+    // response
     #[test]
-    fn test_request_try_from_bytes_content_length_more() {
-        let input = BytesMut::from(
-            "POST / HTTP/1.1\r\nContent-Length: 0\r\n\r\nHello",
-        );
-        let result = OneRequest::try_from(input).unwrap();
-        let verify = BytesMut::from(
-            "POST / HTTP/1.1\r\nContent-Length: 5\r\n\r\nHello",
-        );
-        assert_eq!(result.into_bytes(), verify);
-    }
-
-    #[test]
-    fn test_response_try_from_bytes_content_length_no_cl() {
-        let input = BytesMut::from("HTTP/1.1 200 OK\r\n\r\nHello");
+    fn test_response_try_from_bytes_no_body() {
+        let res = "HTTP/1.1 200 OK\r\n\r\n";
+        let input = BytesMut::from(res);
         let result = OneResponse::try_from(input).unwrap();
-        let verify = BytesMut::from(
-            "HTTP/1.1 200 OK\r\ncontent-length: 5\r\n\r\nHello",
-        );
-        assert_eq!(result.into_bytes(), verify);
+        assert_eq!(result.into_bytes(), res);
     }
 
     #[test]
-    fn test_response_try_from_bytes_content_length_less() {
-        let input =
-            BytesMut::from("HTTP/1.1 200 OK\r\nContent-Length: 10\r\n\r\na");
+    fn test_response_try_from_bytes_no_cl_body() {
+        let res = "HTTP/1.1 200 OK\r\n\r\nHello";
+        let input = BytesMut::from(res);
         let result = OneResponse::try_from(input).unwrap();
-        let verify =
-            BytesMut::from("HTTP/1.1 200 OK\r\nContent-Length: 1\r\n\r\na");
-        assert_eq!(result.into_bytes(), verify);
+        assert_eq!(result.into_bytes(), res);
     }
 
     #[test]
-    fn test_response_try_from_bytes_content_length_more() {
-        let input = BytesMut::from(
-            "HTTP/1.1 200 OK\r\nContent-Length: 0\r\n\r\nHello",
-        );
+    fn test_response_try_from_bytes_cl_body() {
+        let res = "HTTP/1.1 200 OK\r\nContent-Length: 10\r\n\r\na";
+        let input = BytesMut::from(res);
         let result = OneResponse::try_from(input).unwrap();
-        let verify = BytesMut::from(
-            "HTTP/1.1 200 OK\r\nContent-Length: 5\r\n\r\nHello",
-        );
-        assert_eq!(result.into_bytes(), verify);
+        assert_eq!(result.into_bytes(), res);
+    }
+
+    // error
+    #[rstest]
+    #[case("nocrlf")]
+    #[case("nofirstows\r\n\r\n")] // no first ows
+    #[case("no secondows\r\n\r\n")] // no second ows
+    #[case("no info line")] // no info line
+    fn test_try_from_bytes_err(#[case] input: &str) {
+        let result = OneRequest::try_from(BytesMut::from(input)).unwrap_err();
+        dbg!(&result);
+        assert_eq!(result.into_bytes(), input);
     }
 }
