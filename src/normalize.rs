1a9d87635dad55512ae8a255cd2c2d36f2bfcb60
darkseid <gh0stv3g3ta@gmail.com>
2026-04-14 10:16:07 +0530

    fix: Normalize add 'connecion: close' header only for http <2 versions

--- src/normalize.rs
+++ src/normalize.rs
@@ -1,7 +1,8 @@
 use body_plz::variants::Body;
 use bon::Builder;
+use header_plz::Version;
 use header_plz::{
-    OneHeaderMap, OneInfoLine, OneMessageHead,
+    OneInfoLine, OneMessageHead,
     body_headers::parse::ParseBodyHeaders,
     const_headers::{CLOSE, CONNECTION, CONTENT_LENGTH, KEEP_ALIVE},
 };
@@ -21,42 +22,42 @@ pub struct NormalizerOpts {
 }
 
 impl NormalizerOpts {
-    pub fn normalize<T>(&self, message: &mut OneOne<T>)
+    pub fn normalize<T>(&self, msg: &mut OneOne<T>)
     where
         T: OneInfoLine + std::fmt::Debug,
         OneMessageHead<T>: ParseBodyHeaders,
     {
         if self.fix_content_length {
-            Self::fix_content_length(message)
+            Self::fix_content_length(msg)
         }
         if self.close_connection {
-            Self::close_connection(message.header_map_mut())
+            Self::close_connection(msg)
         }
         if self.proxy_connection
-            && let Some(pos) = message.has_proxy_connection()
+            && let Some(pos) = msg.has_proxy_connection()
         {
-            message.remove_header_on_position(pos);
+            msg.remove_header_on_position(pos);
         }
         if self.sec_ws_ext {
-            message.remove_header_on_key(
+            msg.remove_header_on_key(
                 header_plz::const_headers::SEC_WEBSOCKET_EXTENSIONS,
             );
         }
     }
 
-    fn fix_content_length<T>(message: &mut OneOne<T>)
+    fn fix_content_length<T>(msg: &mut OneOne<T>)
     where
         T: OneInfoLine + std::fmt::Debug,
         OneMessageHead<T>: ParseBodyHeaders,
     {
-        let len = if let Some(Body::Raw(body)) = message.body() {
+        let len = if let Some(Body::Raw(body)) = msg.body() {
             body.len()
         } else {
             // https://github.com/curl/curl/issues/13380
             // Adding "Content-Length: 0" is not mandatory.
             return;
         };
-        let headers = message.header_map_mut();
+        let headers = msg.header_map_mut();
         if let Some(index) = headers.header_key_position(CONTENT_LENGTH) {
             headers.update_header_value_on_position(index, len.to_string());
         } else {
@@ -64,13 +65,21 @@ impl NormalizerOpts {
         }
     }
 
-    fn close_connection(headers: &mut OneHeaderMap) {
-        if let Some(index) =
-            headers.header_position((CONNECTION, KEEP_ALIVE.as_bytes()))
+    fn close_connection<T>(msg: &mut OneOne<T>)
+    where
+        T: OneInfoLine + std::fmt::Debug,
+        OneMessageHead<T>: ParseBodyHeaders,
+    {
+        use Version::*;
+        if let Some(index) = msg
+            .header_map_mut()
+            .header_position((CONNECTION, KEEP_ALIVE.as_bytes()))
         {
-            headers.update_header_value_on_position(index, CLOSE);
-        } else {
-            headers.insert(CONNECTION, CLOSE);
+            msg.header_map_mut().update_header_value_on_position(index, CLOSE);
+        } else if let Some(version) = msg.version()
+            && matches!(version, H09 | H10 | H11)
+        {
+            msg.header_map_mut().insert(CONNECTION, CLOSE)
         }
     }
 }
@@ -98,7 +107,7 @@ mod test {
     }
 
     #[test]
-    fn test_normalize_no_close_connection() {
+    fn test_normalize_close_connection() {
         let req = "POST / HTTP/1.1\r\n\
                    Connection: keep-alive\r\n\
                    Content-Length: 1\r\n\r\n\
@@ -115,6 +124,27 @@ mod test {
     }
 
     #[test]
+    fn test_normalize_add_close_connection() {
+        let req = "POST / HTTP/1.1\r\n\r\n";
+        let mut request = OneRequest::try_from(BytesMut::from(req)).unwrap();
+        let normalizer = NormalizerOpts::builder().build();
+        normalizer.normalize(&mut request);
+        let verify = "POST / HTTP/1.1\r\n\
+                      connection: close\r\n\r\n";
+        assert_eq!(request.into_bytes(), verify);
+    }
+
+    #[test]
+    fn test_normalize_add_no_close_connection() {
+        let req = "POST / HTTP/2\r\n\r\n";
+        let mut request = OneRequest::try_from(BytesMut::from(req)).unwrap();
+        let normalizer = NormalizerOpts::builder().build();
+        normalizer.normalize(&mut request);
+        let verify = "POST / HTTP/2\r\n\r\n";
+        assert_eq!(request.into_bytes(), verify);
+    }
+
+    #[test]
     fn test_normalize_no_cl() {
         let req = "POST / HTTP/1.1\r\n\
                    Connection: keep-alive\r\n\
@@ -127,6 +157,21 @@ mod test {
         let verify = "POST / HTTP/1.1\r\n\
                    Connection: close\r\n\
                    Content-Length: 1\r\n\r\n\
+                   hello world";
+        assert_eq!(request.into_bytes(), verify);
+    }
+
+    #[test]
+    fn test_normalize_add_cl() {
+        let req = "POST / HTTP/1.1\r\n\
+                   Connection: keep-alive\r\n\r\n\
+                   hello world";
+        let mut request = OneRequest::try_from(BytesMut::from(req)).unwrap();
+        let normalizer = NormalizerOpts::builder().build();
+        normalizer.normalize(&mut request);
+        let verify = "POST / HTTP/1.1\r\n\
+                   Connection: close\r\n\
+                   content-length: 11\r\n\r\n\
                    hello world";
         assert_eq!(request.into_bytes(), verify);
     }
